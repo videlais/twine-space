7e9a52f44e6a3041773f65f56e3da9f5b863383c
Dan Cox <daniel.57@gmail.com>
2022-07-07 15:34:39 -0400

    Re-built Passage

--- test/Passage.test.js
+++ test/Passage.test.js
@@ -1,23 +1,17 @@
 const Passage = require('../src/Passage.js');
 
 describe('constructor()', () => {
-  it('Should contain default id when initialized with no arguments', () => {
-    const p = new Passage();
-    expect(p.id).toBe(1);
-  });
-
-  it('Should contain default name when initialized with no arguments', () => {
+  it('Should contain default values when initialized with no arguments', () => {
     const p = new Passage();
     expect(p.name).toBe('Default');
-  });
-
-  it('Should contain default tags when initialized with no arguments', () => {
-    const p = new Passage();
     expect(p.tags.length).toBe(0);
+    expect(p.source).toBe('');
   });
 
-  it('Should contain default source when initialized with no arguments', () => {
-    const p = new Passage();
-    expect(p.source).toBe('');
+  it('Should have passed in arguments', () => {
+    const p = new Passage('test', ['tag1'], 'test');
+    expect(p.source).toBe('test');
+    expect(p.name).toBe('test');
+    expect(p.tags.length).toBe(1);
   });
 });
