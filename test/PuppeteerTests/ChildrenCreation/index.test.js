100d6e3d699fb345d3a4f752b3ea93555a4a76d4
Dan Cox <daniel.57@gmail.com>
2022-07-12 01:15:47 -0400

    Entity and chilren creation tests

--- test/PuppeteerTests/ChildrenCreation/index.test.js
+++ test/PuppeteerTests/ChildrenCreation/index.test.js
@@ -4,23 +4,23 @@
 const ShellJS = require('shelljs');
 const path = require('path');
 require('expect-puppeteer');
-    
+      
 ShellJS.exec(`extwee -c -s docs/dist/aframe-format.js -i ${path.join(__dirname, 'index.twee')} -o ${path.join(__dirname, 'index.html')}`);
-     
-describe('Puppeteer Tests - Canvas Generation', () => {
+       
+describe('Puppeteer Tests - Entity Generation', () => {
     beforeAll(async () => {
         await page.goto(`file://${path.join(__dirname, 'index.html')}`);
     });
-   
+     
     afterAll(async () => {
         ShellJS.rm(`${path.join(__dirname, 'index.html')}`);
     });
-    
-    it('Should create <canvas> element', async () => {
+      
+    it('Should create <a-entity> > <a-text> elements', async () => {
         let elementExists = false;
         elementExists = await page.evaluate(() => {
-            return document.querySelector("canvas") !== null;
-          })
+            return document.querySelector("a-entity > a-text") !== null;
+        })
         await expect(elementExists).toBe(true);
-    });
-});
+     });
+ });
--- test/PuppeteerTests/EntityCreation/index.test.js
+++ test/PuppeteerTests/EntityCreation/index.test.js
@@ -4,23 +4,23 @@
 const ShellJS = require('shelljs');
 const path = require('path');
 require('expect-puppeteer');
-    
-ShellJS.exec(`extwee -c -s docs/dist/aframe-format.js -i ${path.join(__dirname, 'index.twee')} -o ${path.join(__dirname, 'index.html')}`);
      
-describe('Puppeteer Tests - Canvas Generation', () => {
+ShellJS.exec(`extwee -c -s docs/dist/aframe-format.js -i ${path.join(__dirname, 'index.twee')} -o ${path.join(__dirname, 'index.html')}`);
+      
+describe('Puppeteer Tests - Entity Generation', () => {
     beforeAll(async () => {
         await page.goto(`file://${path.join(__dirname, 'index.html')}`);
     });
-   
+    
     afterAll(async () => {
         ShellJS.rm(`${path.join(__dirname, 'index.html')}`);
     });
-    
-    it('Should create <canvas> element', async () => {
+     
+    it('Should create <a-text> element', async () => {
         let elementExists = false;
         elementExists = await page.evaluate(() => {
-            return document.querySelector("canvas") !== null;
-          })
+            return document.querySelector("a-text") !== null;
+        })
         await expect(elementExists).toBe(true);
     });
 });
