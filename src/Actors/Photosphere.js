2a60b223321946b9b40e6289e3375a5f3032ea49
Dan Cox <daniel.57@Gmail.com>
2024-02-15 19:33:57 -0500

    Photosphere support

--- src/Actors/Photosphere.js
+++ src/Actors/Photosphere.js
@@ -12,7 +12,7 @@ import { PhotoDome } from '@babylonjs/core/Helpers/photoDome.js';
   * @param {number} options.resolution - The resolution of the photosphere.
   * @param {number} options.size - The size of the photosphere.
   */
-export default function Photosphere(name, url, options) {
+export default async function Photosphere(name, url, options) {
 
   // Prepare default mesh.
   let mesh = null;
@@ -23,35 +23,77 @@ export default function Photosphere(name, url, options) {
       name = '';
     }
 
-    // Check if url is a string.
-    if(typeof url !== 'string') {
-      url = '';
-    }
+    // Function to continue loading.
+    // This function will be called after the image has loaded.
+    const continueLoading = () => {
+      // Prepare default resolution and size values.
+      let resolution = 32;
+      let size = 1000;
 
-    // Prepare default resolution and size values.
-    let resolution = 32;
-    let size = 1000;
+      // Check if options is an object.
+      if(typeof options === 'object') {
 
-    // Check if options is an object.
-    if(typeof options === 'object') {
-      // Check if options.resolution is a number.
-      if(typeof options.resolution === 'number') {
-        resolution = options.resolution;
+        // Check if options.resolution property exists.
+        if(Object.prototype.hasOwnProperty.call(options, 'resolution')) {
+          // Check if options.resolution is a number.
+          if(typeof options.resolution === 'number') {
+            resolution = options.resolution;
+          }
+        }
+
+        // Check if options.size property exists.
+        if(Object.prototype.hasOwnProperty.call(options, 'size')) {
+          // Check if options.size is a number.
+          if(typeof options.size === 'number') {
+            size = options.size;
+          }
+        }
       }
 
-      // Check if options.size is a number.
-      if(typeof options.size === 'number') {
-        size = options.size;
-      }
+      // Create photosphere. (Photodome.)
+      mesh = new PhotoDome(name, url,
+        {
+          resolution: resolution,
+          size: size
+        },
+      );
     }
 
-    // Create photosphere. (Photodome.)
-    mesh = new PhotoDome(name, url,
-      {
-        resolution: resolution,
-        size: size
-      },
-    );
+    // Promise-based image loader
+    const loadImage = path => {
+      return new Promise((resolve) => {
+        // Create a new image.
+        const img = new Image();
+
+        // Set the crossOrigin property to 'Anonymous'.
+        img.crossOrigin = 'Anonymous';
+
+        // Set the source of the image.
+        img.src = path;
+
+        // Set the onLoad.
+        img.onload = () => {
+          console.log('Image loaded:', img.src);
+          resolve(true);
+        };
+        
+        // Set the onError.
+        img.onerror = e => {
+          console.error('Image failed to load:', img.src);
+          resolve(false);
+        };
+      })
+    }
+
+    // Check if the url is an image.
+    const isImage = await loadImage(url);
+
+    // Continue loading if the url is an image.
+    if(isImage) {
+      // Continue loading.
+      continueLoading();
+    }
+
   }
 
   // Return photosphere or null.
