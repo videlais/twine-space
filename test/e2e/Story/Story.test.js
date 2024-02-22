1d7b254b20b15cd23b7ee00f75b6ce59e2d7359f
Dan Cox <daniel.57@Gmail.com>
2024-02-21 23:30:15 -0500

    Adding testing TODO items for Story

--- test/e2e/Story/Story.test.js
+++ test/e2e/Story/Story.test.js
@@ -1,4 +1,4 @@
-// Description: Test for Story.js
+// Description: E2E Test for Story.js
 import shell from 'shelljs';
 import 'expect-puppeteer';
 
@@ -29,6 +29,18 @@ describe('Story', () => {
 
     expect(name).toBe('Story');
   });
+
+  it.todo('should find tw-storydata element');
+  it.todo('should find tw-story element');
+  it.todo('should find tw-passage element');
+  it.todo('should apply style rules found in type="text/twine-css"');
+  it.todo('should apply script found in type="text/twine-javascript"');
+  it.todo('should load passages');
+  it.todo('should load starting passage');
+  it.todo('should navigate between passages by link');
+  it.todo('should navigate to a passage by name');
+  it.todo('should load Actors based on YAML in passages');
+  it.todo('should clear meshes and load new Actors when navigating between passages');
 
   afterAll(() => {
     // Remove the bundle.
